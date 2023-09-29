21f446cd3ca5acf8222fa9d357147696e87d77ad
dmproj <seyav29@gmail.com>
2023-09-29 19:03:05 -0400

    5

---
+++ hikitocAPI/Controllers/SolarSystemsController.cs
@@ -0,0 +1,26 @@
+﻿using hikitocAPI.Data;
+using hikitocAPI.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hikitocAPI.Controllers
+{
+    [Route("api/[controller]")] // localhost:port/api/solarsystems
+    [ApiController]
+    public class SolarSystemsController : ControllerBase
+    {
+        private readonly HikitocDbContext hikitocDbContext;
+
+        public SolarSystemsController(HikitocDbContext hikitocDbContext)
+        {
+            this.hikitocDbContext = hikitocDbContext;
+        }
+        //GET ALL SOLAR SYSTEMS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var solarSystems = hikitocDbContext.SolarSystems.ToList();
+            return Ok(new { Message = "Success from controller", Data = solarSystems });
+        }
+    }
+}
