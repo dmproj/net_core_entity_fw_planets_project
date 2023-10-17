ee243d950c885ad6092c89190b562d21267d5d46
dmproj <seyav29@gmail.com>
2023-10-17 19:34:40 -0400

    all SolrSystems Controller Action Methods are updated with async await and try catch blocks to handle various exceptions.

--- hikitocAPI/Controllers/SolarSystemsController.cs
+++ hikitocAPI/Controllers/SolarSystemsController.cs
@@ -16,77 +16,111 @@ namespace hikitocAPI.Controllers
         {
             this.hikitocDbContext = hikitocDbContext;
         }
+
         //GET ALL SOLAR SYSTEMS
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var solarSystems = hikitocDbContext.SolarSystems.ToList();
-
-            var solarSystemsDto = solarSystems.Select(solarSystem => new SolarSystemDto()
+            try
             {
-                Id = solarSystem.Id,
-                Code = solarSystem.Code,
-                Name = solarSystem.Name,
-                Image = solarSystem.Image,
-            }).ToList();
+                var solarSystems = await hikitocDbContext.SolarSystems.ToListAsync();
 
-            return Ok(new { Message = "Success from GetAll action method", Data = solarSystemsDto });
+                var solarSystemsDto = solarSystems.Select(solarSystem => new SolarSystemDto()
+                {
+                    Id = solarSystem.Id,
+                    Code = solarSystem.Code,
+                    Name = solarSystem.Name,
+                    Image = solarSystem.Image,
+                }).ToList();
+
+                return Ok(new { Message = "Success from GetAll action method", Data = solarSystemsDto });
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new { Message = "Database error occurred", Error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred", Error = ex.Message });
+            }
         }
+
 
         //GET 1 SOLAR SYSTEM BY ID
         //[HttpGet("{id}")]
-
         [HttpGet]
         [Route("{id:Guid}")] // localhost:port/api/solarsystems/{id}
-        public IActionResult GetById([FromRoute] Guid id)
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
-            var solarSystemSingle = hikitocDbContext.SolarSystems.SingleOrDefault(item => item.Id == id);
-
-            if (solarSystemSingle == null)
+            try
             {
-                return NotFound(new { Message = "No Solar System found!" });
+                var solarSystemSingle = await hikitocDbContext.SolarSystems.SingleOrDefaultAsync(item => item.Id == id);
+
+                if (solarSystemSingle == null)
+                {
+                    return NotFound(new { Message = "No Solar System found!" });
+                }
+
+                var solarSystemDto = new SolarSystemDto
+                {
+                    Id = solarSystemSingle.Id,
+                    Code = solarSystemSingle.Code,
+                    Name = solarSystemSingle.Name,
+                    Image = solarSystemSingle.Image,
+                };
+
+                return Ok(new { Message = "1 Solar System found!", Data = solarSystemDto });
             }
-
-            var solarSystemDto = new SolarSystemDto
+            catch (DbUpdateException ex)
             {
-                Id = solarSystemSingle.Id,
-                Code = solarSystemSingle.Code,
-                Name = solarSystemSingle.Name,
-                Image = solarSystemSingle.Image,
-            };
-
-            return Ok(new { Message = "1 Solar System found!", Data = solarSystemDto });
+                return StatusCode(500, new { Message = "Database update error occurred", Error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred", Error = ex.Message });
+            }
         }
+
 
         //POST/INSERT A SOLAR SYSTEM
-
         [HttpPost] // localhost:port/api/solarsystems/
-        public IActionResult InsertSingle([FromBody] InsertSolarSystemDto insertSolarSystemDto)
+        public async Task<IActionResult> InsertSingle([FromBody] InsertSolarSystemDto insertSolarSystemDto)
         {
-            var solarSystem = new SolarSystem
+            try
             {
-                Code = insertSolarSystemDto.Code,
-                Name = insertSolarSystemDto.Name,
-                Image = insertSolarSystemDto.Image,
-            };
+                var solarSystem = new SolarSystem
+                {
+                    Code = insertSolarSystemDto.Code,
+                    Name = insertSolarSystemDto.Name,
+                    Image = insertSolarSystemDto.Image,
+                };
 
-            hikitocDbContext.SolarSystems.Add(solarSystem);
-            hikitocDbContext.SaveChanges();
+                hikitocDbContext.SolarSystems.Add(solarSystem);
+                await hikitocDbContext.SaveChangesAsync();
 
-            var solarSystemDto = new SolarSystemDto
+                var solarSystemDto = new SolarSystemDto
+                {
+                    Id = solarSystem.Id,
+                    Code = solarSystem.Code,
+                    Name = solarSystem.Name,
+                    Image = solarSystem.Image,
+                };
+
+                return Created("/api/solarsystems/" + solarSystem.Id, new { Message = "Solar System created!", Data = solarSystemDto });
+            }
+            catch (DbUpdateException ex)
             {
-                Id = solarSystem.Id,
-                Code = solarSystem.Code,
-                Name = solarSystem.Name,
-                Image = solarSystem.Image,
-            };
-
-            return Created("/api/solarsystems/" + solarSystem.Id, new { Message = "Solar System created!", Data = solarSystemDto });
+                return StatusCode(500, new { Message = "Database update error occurred", Error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred", Error = ex.Message });
+            }
         }
+
 
         //PUT/UPDATE 1 SOLAR SYSTEM BY ID
         //[HttpPut("{id}")]
-
         [HttpPut]
         [Route("{id:Guid}")] // localhost:port/api/solarsystems/{id}
         public async Task<IActionResult> UpdateById([FromRoute] Guid id, [FromBody] UpdateSolarSystemDto updateSolarSystemDto)
@@ -134,7 +168,6 @@ namespace hikitocAPI.Controllers
 
         //DELETE A SOLAR SYSTEM BY ID
         //[HttpDelete("{id}")]
-
         [HttpDelete]
         [Route("{id:Guid}")] // localhost:port/api/solarsystems/{id}
         public async Task<IActionResult> DeleteById([FromRoute] Guid id)
@@ -168,6 +201,6 @@ namespace hikitocAPI.Controllers
                 return StatusCode(500, new { Message = "An error occured while processing the request" });
             }
         }
+
     }
 }
-
---
+++ hikitocAPI/Models/Domain/MyPoco.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace hikitocAPI.Models.Domain
+{
+    public class MyPoco
+    {
+        public int? Vs { get; set; }
+        public string? Auth { get; set; }
+
+        public override string ToString()
+        {
+            return $"replies, Vs: {Vs}, Auth: {Auth}";
+        }
+
+    }
+}
